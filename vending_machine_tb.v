d202bb5e94c55ffabb14a6d074b81f8c5adbdd32
sandippaulece <paulsandip712146@gmail.com>
2025-11-04 15:36:47 +0530

    Update vending_machine_tb.v

--- vending_machine_tb.v
+++ vending_machine_tb.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.11.2025 20:50:22
-// Design Name: 
-// Module Name: vending_machine_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module vending_machine_tb;
 
     // Inputs
